68401f8f09e102be260defa8e39a91d8547412d7
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-07-26 17:30:32 +0530

    Added My first nuxt app data

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Hello How r u</h1>
+    <h2>{{state.count}}</h2>
+    <button @click="increment">
+  {{ state.count }}
+</button>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+export default {
+     setup() {
+    const state = reactive({ count: 0 })
+      function increment() {
+      state.count++
+    }
+
+    // expose the state to the template
+    return {
+      state,
+      increment
+    }
+  }
+
+    
+}
+</script>
+
+<style>
+
+</style>
