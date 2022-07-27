96cc77efe4970cdd66ec081c259b8d1a714d829b
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-07-27 12:57:17 +0530

    Style binding in nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,62 @@
 <template>
   <div>
     <h1>Hello How r u</h1>
-    <h2>{{state.count}}</h2>
-    <button @click="increment">
-    <input type="text" placeholder="fname" v-model="mydata.fname">
-    <input type="text" placeholder="add" v-model="mydata.add">
-    <input type="number" placeholder="mobileno" v-model="mydata.mobileno">
-    <p>My information is:{{mydata.info}}</p>
- click
-</button>
+    <h2>{{ state.count }}</h2>
+    <button @click="increment">click</button>
+    <input type="text" placeholder="fname" v-model="mydata.fname" />
+    <input type="text" placeholder="add" v-model="mydata.add" />
+    <input type="number" placeholder="mobileno" v-model="mydata.mobileno" />
+    <input type="color" v-model="state.mycolor"/>
+    <p :style="{color: state.mycolor}" id="myname">My information is:{{ mydata.info }}</p>
+    <button @click="ChangeColor()">change color</button>
+
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive } from "vue";
 
-const state = reactive({ count: 0 })
+// const state = reactive({ count: 0 });
 export default {
-     setup() {
-    const state = reactive({ count: 0 })
-      function increment() {
-      state.count++
+  setup() {
+    const state = reactive({count: 0});
+
+    function ChangeColor() {
+      document.getElementById("myname").style.color = "#FF0000";
     }
-    const mydata=reactive({
-      fname:'vaibhav',
-      add:'pune',
-      mobileno:9876532234,
-      info:computed(()=> {
-        return mydata.fname+ ' ' + mydata.add + ' ' + mydata.mobileno + ' ' + "Sambhajinagar";
-      })
-    })
+
+
+
+    function increment() {
+      state.count++;
+    }
+    const mydata = reactive({
+      mycolor:'#00ff00',
+      fname: "vaibhav",
+      add: "pune",
+      mobileno: 9876532234,
+      info: computed(() => {
+        return (
+          mydata.fname +
+          " " +
+          mydata.add +
+          " " +
+          mydata.mobileno +
+          " " +
+          "Sambhajinagar"
+        );
+      }),
+    });
 
     // expose the state to the template
     return {
       state,
       increment,
-      mydata
-    }
-  }
-
-    
-}
+      mydata,
+      ChangeColor
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
