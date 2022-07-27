62eb1e3aaced96ecd038f7a23b358a9680c5589d
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-07-27 11:32:57 +0530

    add object using reactive and using ref add first name

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,10 @@
     <h1>Hello How r u</h1>
     <h2>{{state.count}}</h2>
     <button @click="increment">
+    <input type="text" placeholder="fname" v-model="mydata.fname">
+    <input type="text" placeholder="add" v-model="mydata.add">
+    <input type="number" placeholder="mobileno" v-model="mydata.mobileno">
+    <p>My information is:{{mydata.fname + ' ' + mydata.add + ' ' +mydata.mobileno}}</p>
  click
 </button>
   </div>
@@ -18,11 +22,17 @@ export default {
       function increment() {
       state.count++
     }
+    const mydata=reactive({
+      fname:'vaibhav',
+      add:'pune',
+      mobileno:9876532234
+    })
 
     // expose the state to the template
     return {
       state,
-      increment
+      increment,
+      mydata
     }
   }
 
